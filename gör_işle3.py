4bdd3db016729d1bc491cc262b4793ce68e21940
StartaBafras <30952126+StartaBafras@users.noreply.github.com>
2020-02-14 20:33:29 +0300

    Add files via upload

---
+++ gör_işle3.py
@@ -0,0 +1,24 @@
+def ele():
+    import cv2
+    import numpy as np
+    resim = cv2.imread("Harf1.jpg")
+    cv2.imshow("de",resim)
+    kes = resim[0:140,0:370]
+    cv2.imshow("d",kes)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+import cv2
+import pytesseract
+from PIL import Image
+
+img_cv = cv2.imread("Harf1.jpg")
+
+# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
+# we need to convert from BGR to RGB format/mode:
+#img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+#print(pytesseract.image_to_string(img_rgb))
+# OR
+img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
+print(pytesseract.image_to_string(img_rgb))
